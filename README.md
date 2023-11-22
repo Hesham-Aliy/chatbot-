d486575a52321446c4b4769d505c1fe86bafb9b1
Hesham Ali <76598242+Hesham-Aliy@users.noreply.github.com>
2023-11-22 17:32:37 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Chat completion from openai
+This task is about using openAI API to do chat completion using python3. 
+
+
+
+## How to install and run
+#### Get your api key from api home page of openAI website, then replace the key in the main.py with your key
+
+
+
+#### Run the taining job script
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+python3 main.py
+```
+
+
+
---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import openai
+
+#Put your key:
+openai.api_key = 'sk-HbWsgb1---------'
+
+def chat_completion(prompt):
+    # Define the prompt for the chat-based completion
+    prompt = f"You are a helpful assistant.\n\nUser: {prompt}\nAssistant:"
+
+    # Call the OpenAI API for chat-based completion using OpenAI API v1.0.0
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # You can choose other engines like text-davinci-003
+        prompt=prompt,
+        max_tokens=150  # Adjust max_tokens as needed
+    )
+
+    # Extract the assistant's reply from the API response
+    assistant_reply = response['choices'][0]['text'].strip()
+    return assistant_reply
+
+# Example usage
+user_input = "The Capital of Egypt is ...."
+response = chat_completion(user_input)
+print(f"User: {user_input}\nAssistant: {response}")
+
